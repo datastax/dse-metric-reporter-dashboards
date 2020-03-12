2d6a45b3a73cdca955903f63892f8f28703f94a9
Christopher Bradford <christopher.bradford@datastax.com>
2020-03-11 22:44:06 -0400

    Adding output to build scripts

--- k8s-build/bin/build.py
+++ k8s-build/bin/build.py
@@ -6,30 +6,40 @@ import os
 import shutil
 import yaml
 
-output_path = "generated"
+base_path = os.path.join(os.path.dirname(__file__), "..")
+generated_path = os.path.join(base_path, "generated")
 
 
 # Helper method to allow for `literal` YAML syntax
 def str_presenter(dumper, data):
-    if len(data.splitlines()) > 1:    # check for multiline string
+    if len(data.splitlines()) > 1: # check for multiline string
         return dumper.represent_scalar('tag:yaml.org,2002:str', data, style='|')
     return dumper.represent_scalar('tag:yaml.org,2002:str', data)
 
 
 yaml.add_representer(str, str_presenter)
 
-os.mkdir(output_path)
+os.mkdir(generated_path)
 
 # Grafana
 # Copy files
-shutil.copytree(os.path.join("templates", "grafana"), os.path.join(output_path, "grafana"))
+grafana_dashboard_path = os.path.join(base_path, "..", "grafana", "dashboards")
+grafana_template_path = os.path.join(base_path, "templates", "grafana")
+grafana_output_path = os.path.join(generated_path, "grafana")
+
+print("Grafana")
+print("=======")
+print("Copying templates to output path")
+print(f"{grafana_template_path} => {grafana_output_path}")
+shutil.copytree(grafana_template_path, grafana_output_path)
 
 # Load k8s dashboard template
-with open(os.path.join(output_path, "grafana", "dashboard.yaml"), "r") as template_file:
+dashboard_template_path = os.path.join(grafana_output_path, "dashboard.yaml")
+with open(dashboard_template_path, "r") as template_file:
     k8s_template = yaml.safe_load(template_file)
 
     # Iterate over all dashboards
-    dashboards = glob(os.path.join("..", "grafana", "dashboards", "*.json"))
+    dashboards = glob(os.path.join(grafana_dashboard_path, "*.json"))
     for json_dashboard in dashboards:
         # Copy the template and update with the appropriate values
         k8s_dashboard = deepcopy(k8s_template)
@@ -41,13 +51,19 @@ with open(os.path.join(output_path, "grafana", "dashboard.yaml"), "r") as templa
             k8s_dashboard["spec"]["json"] = json_file.read()
 
         # Write out the k8s dashboard file
-        dashboard_path = os.path.join(output_path, "grafana", f"{k8s_dashboard['metadata']['name']}.dashboard.yaml")
-        with open(dashboard_path, "w") as k8s_file:
+        dashboard_filename = f"{k8s_dashboard['metadata']['name']}.dashboard.yaml"
+        dashboard_output_path = os.path.join(generated_path, "grafana", dashboard_filename)
+
+        print(f"Templating {json_dashboard} => {dashboard_output_path}")
+
+        with open(dashboard_output_path, "w") as k8s_file:
             k8s_file.write(yaml.dump(k8s_dashboard))
 
 # Delete original template from distribution
-os.remove(os.path.join(output_path, "grafana", "dashboard.yaml"))
-
+print("Removing template from generated directory")
+print(dashboard_template_path)
+os.remove(os.path.join(grafana_output_path, "dashboard.yaml"))
+print("")
 
 # Prometheus
 key_mapping = {
@@ -60,14 +76,23 @@ key_mapping = {
 }
 
 # Copy files
-shutil.copytree(os.path.join("templates", "prometheus"), os.path.join(output_path, "prometheus"))
+prometheus_output_path = os.path.join(generated_path, "prometheus")
+prometheus_template_path = os.path.join(base_path, "templates", "prometheus")
+service_monitor_path = os.path.join(prometheus_output_path, "service_monitor.yaml")
+prometheus_config_path = os.path.join(base_path, "..", "prometheus", "prometheus.yml")
+
+print("Prometheus")
+print("=======")
+print("Copying templates to output path")
+print(f"{prometheus_template_path} => {prometheus_output_path}")
+shutil.copytree(prometheus_template_path, prometheus_output_path)
 
 # Load k8s service monitor template
-with open(os.path.join(output_path, "prometheus", "service_monitor.yaml"), "r") as template_file:
+with open(service_monitor_path, "r") as template_file:
     k8s_service_monitor = yaml.safe_load(template_file)
 
     # Load prometheus configuration file
-    with open(os.path.join("..", "prometheus", "prometheus.yml"), "r") as prometheus_file:
+    with open(prometheus_config_path, "r") as prometheus_file:
         prometheus_conf = yaml.safe_load(prometheus_file)
 
         # Extract scrape configs
@@ -86,6 +111,8 @@ with open(os.path.join(output_path, "prometheus", "service_monitor.yaml"), "r") 
 
                     k8s_service_monitor['spec']['endpoints'][0]['metricRelabelings'].append(k8s_relabel_config)
 
-        # Write out templated k8s service monitor
-    with open(os.path.join(output_path, "prometheus", "service_monitor.yaml"), "w") as service_monitor_file:
+    # Write out templated k8s service monitor
+    with open(service_monitor_path, "w") as service_monitor_file:
+        print("Writing out service monitor configuration")
+        print(service_monitor_path)
         yaml.dump(k8s_service_monitor, service_monitor_file)
--- k8s-build/bin/clean.py
+++ k8s-build/bin/clean.py
@@ -3,5 +3,9 @@
 import shutil
 import os
 
-if os.path.isdir("generated"):
-    shutil.rmtree("generated")
+base_path = os.path.join(os.path.dirname(__file__), "..")
+generated_path = os.path.join(base_path, "generated")
+
+if os.path.isdir(generated_path):
+    print(f"Removing {generated_path}")
+    shutil.rmtree(generated_path)
